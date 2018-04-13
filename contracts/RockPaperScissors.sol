a3035ec809c042e80ed78a63f6e870eff63c52ba
Job Joseph <job_joseph@hotmail.com>
2018-04-13 16:26:06 -0500

    use storage

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -36,8 +36,8 @@ contract RockPaperScissors is ActiveState {
 
     function getGameProgression(address opponent) public isActiveContract returns (uint gameProgression, address, bytes32,  address, bytes32) {
         bytes32 hashValue = getHashForUniqueGame(msg.sender,opponent);
-        rockPaperScissorsGameStruct gameRecord = rockPaperScissorsGame[hashValue];
-        return (uint(gameRecord.gameProgression),msg.sender,rockPaperScissorsGame[hashValue].playerChoice[msg.sender].hiddenToolChoice,opponent,rockPaperScissorsGame[hashValue].playerChoice[opponent].hiddenToolChoice);
+        rockPaperScissorsGameStruct storage gameRecord = rockPaperScissorsGame[hashValue];
+        return (uint(gameRecord.gameProgression),msg.sender,gameRecord.playerChoice[msg.sender].hiddenToolChoice,opponent,gameRecord.playerChoice[opponent].hiddenToolChoice);
     }
     /*
         User will pick opponent they want to play against and submit their tool as hidden hash value.
@@ -52,21 +52,22 @@ contract RockPaperScissors is ActiveState {
 
         // All games between two accounts will have a unique hash value for identification purposes.
         bytes32 hashValue = getHashForUniqueGame(msg.sender,opponent);
-        require(rockPaperScissorsGame[hashValue].gameProgression < GameProgression.BothPlayersPickedHiddenTool);
+        rockPaperScissorsGameStruct storage gameRecord = rockPaperScissorsGame[hashValue];
+        require(gameRecord.gameProgression < GameProgression.BothPlayersPickedHiddenTool);
 
         // Tool choice will be saved in hash form.
-        rockPaperScissorsGame[hashValue].playerChoice[msg.sender].hiddenToolChoice = hiddenTool;
-        rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount = totalToWager;   
+        gameRecord.playerChoice[msg.sender].hiddenToolChoice = hiddenTool;
+        gameRecord.playerChoice[msg.sender].amount = totalToWager;   
         // Zero out balance since it will be used in current game.
         winnings[msg.sender] = 0;
 
-        if (rockPaperScissorsGame[hashValue].playerChoice[opponent].hiddenToolChoice == 0x0)
+        if (gameRecord.playerChoice[opponent].hiddenToolChoice == 0x0)
         {
-            rockPaperScissorsGame[hashValue].gameProgression = GameProgression.OnePlayerPickedHiddenTool;
+            gameRecord.gameProgression = GameProgression.OnePlayerPickedHiddenTool;
         }
         else
         {
-            rockPaperScissorsGame[hashValue].gameProgression = GameProgression.BothPlayersPickedHiddenTool;
+            gameRecord.gameProgression = GameProgression.BothPlayersPickedHiddenTool;
         }
     }
 
@@ -78,81 +79,82 @@ contract RockPaperScissors is ActiveState {
     function revealHiddenTool(address opponent, string secretCode) public isActiveContract returns (bool success) {
         require(opponent != address(0));
         bytes32 hashValue = getHashForUniqueGame(msg.sender,opponent);
-        require(rockPaperScissorsGame[hashValue].gameProgression == GameProgression.BothPlayersPickedHiddenTool || rockPaperScissorsGame[hashValue].gameProgression == GameProgression.OnePlayerRevealedTool);
+        rockPaperScissorsGameStruct storage gameRecord = rockPaperScissorsGame[hashValue];
+        require(gameRecord.gameProgression == GameProgression.BothPlayersPickedHiddenTool || gameRecord.gameProgression == GameProgression.OnePlayerRevealedTool);
 
-        bytes32 hiddenToolChoice = rockPaperScissorsGame[hashValue].playerChoice[msg.sender].hiddenToolChoice;
+        bytes32 hiddenToolChoice = gameRecord.playerChoice[msg.sender].hiddenToolChoice;
         if (getHashForSecretlyPickingTool(1,secretCode,msg.sender)==hiddenToolChoice)
         {
-            rockPaperScissorsGame[hashValue].playerChoice[msg.sender].toolChoice = ToolChoice.rock;
+            gameRecord.playerChoice[msg.sender].toolChoice = ToolChoice.rock;
         }
         else if (getHashForSecretlyPickingTool(2,secretCode,msg.sender)==hiddenToolChoice)
         {
-            rockPaperScissorsGame[hashValue].playerChoice[msg.sender].toolChoice = ToolChoice.scissors;
+            gameRecord.playerChoice[msg.sender].toolChoice = ToolChoice.scissors;
         }
         else if (getHashForSecretlyPickingTool(3,secretCode,msg.sender)==hiddenToolChoice)
         {
-            rockPaperScissorsGame[hashValue].playerChoice[msg.sender].toolChoice = ToolChoice.paper;
+            gameRecord.playerChoice[msg.sender].toolChoice = ToolChoice.paper;
         }
         else
         {
             throw;
         }
 
-        if (rockPaperScissorsGame[hashValue].gameProgression == GameProgression.BothPlayersPickedHiddenTool)
+        if (gameRecord.gameProgression == GameProgression.BothPlayersPickedHiddenTool)
         {
-            rockPaperScissorsGame[hashValue].gameProgression = GameProgression.OnePlayerRevealedTool;
+            gameRecord.gameProgression = GameProgression.OnePlayerRevealedTool;
         }
         else
         {
-            ToolChoice toolChoice = rockPaperScissorsGame[hashValue].playerChoice[msg.sender].toolChoice;
-            ToolChoice opponentToolChoice = rockPaperScissorsGame[hashValue].playerChoice[opponent].toolChoice;
+            ToolChoice toolChoice = gameRecord.playerChoice[msg.sender].toolChoice;
+            ToolChoice opponentToolChoice = gameRecord.playerChoice[opponent].toolChoice;
 
             if (toolChoice == ToolChoice.rock)
             {
                 if (opponentToolChoice == ToolChoice.rock)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount;
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount;
+                    winnings[opponent] += gameRecord.playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.scissors)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.paper)
                 {
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[opponent] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
             }
             else if (toolChoice == ToolChoice.scissors)
             {
                 if (opponentToolChoice == ToolChoice.rock)
                 {
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[opponent] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.scissors)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount;
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount;
+                    winnings[opponent] += gameRecord.playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.paper)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
             }
             else if (toolChoice == ToolChoice.paper)
             {
                 if (opponentToolChoice == ToolChoice.rock)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.scissors)
                 {
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[opponent] += gameRecord.playerChoice[msg.sender].amount + rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
                 }
                 else if (opponentToolChoice == ToolChoice.paper)
                 {
-                    winnings[msg.sender] += rockPaperScissorsGame[hashValue].playerChoice[msg.sender].amount;
-                    winnings[opponent] += rockPaperScissorsGame[hashValue].playerChoice[opponent].amount;
+                    winnings[msg.sender] += gameRecord.playerChoice[msg.sender].amount;
+                    winnings[opponent] += gameRecord.playerChoice[opponent].amount;
                 }
             }
 
